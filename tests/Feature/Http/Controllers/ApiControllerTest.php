d2b2f8042c056a1751e2d30e9ccacbbfc3488484
marco <m.baroncini@cyberway.cloud>
2022-12-29 18:56:34 +0100

    Add store route job creation test

--- tests/Feature/Http/Controllers/ApiControllerTest.php
+++ tests/Feature/Http/Controllers/ApiControllerTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\Models\HoquJob;
 use Tests\TestCase;
+use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -15,13 +17,29 @@ class ApiControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_store()
+    public function test_store_route_existence()
     {
         $response = $this->postJson('/api/store');
 
         $response->assertUnauthorized()
             ->assertJson(
                 fn (AssertableJson $json) => $json->hasAll(['message'])
+            );
+    }
+
+    public function test_store_jobs_creation()
+    {
+
+        $user = User::first();
+        $data = ['name' => 'AddLocationsToPoint', 'input' => [
+            "type" => "Point",
+            "coordinates" => [1, 2]
+        ]];
+        $response = $this->actingAs($user)->postJson('/api/store', $data);
+
+        $response->assertStatus(200)
+            ->assertJson(
+                fn (AssertableJson $json) => $json->hasAll(['message', 'job_id'])
             );
     }
 }
